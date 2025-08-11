e8e01e81281cc2555374b7c5e6a758f1760e08f6
ssrathi05 <rathishreya2005@gmail.com>
2025-08-11 16:31:16 -0400

    hi

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,6 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, route } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+export default [
+  route({ path: 'dashboard', file: 'routes/admin/dashboard.tsx' })
+
+] satisfies RouteConfig;
