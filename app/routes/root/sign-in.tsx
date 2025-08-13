3ab121b0e58e508645e7928f16a2d2ef57960803
ssrathi05 <rathishreya2005@gmail.com>
2025-08-13 16:44:44 -0400

    finished sign-in

--- app/routes/root/sign-in.tsx
+++ app/routes/root/sign-in.tsx
@@ -7,7 +7,7 @@ import { account } from "~/appwrite/client";
 export async function clientLoader() {
   try {
     const user = await account.get();
-    if (!user.$id) return redirect("/");
+    if (user.$id) return redirect("/");
   } catch (e) {
     console.log("Error fetching user", e);
   }
